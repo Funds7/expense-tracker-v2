5bf2f2cbe7db61e8ab9db55b01cd570ebe725ec3
Funds7 <odigwejoshua7@gmail.com>
2026-05-08 18:07:58 +0100

    Enhance balance update with income and expense totals
    Add income and expense tracking to the balance update.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+localStorage.clear();
 const form = document.getElementById("transaction-form");
 const title = document.getElementById("title");
 const amount = document.getElementById("amount");
 const type = document.getElementById("type");
 const list = document.getElementById("list");
 const balance = document.getElementById("balance");
+
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
 
 let transactions =
   JSON.parse(localStorage.getItem("transactions")) || [];
@@ -16,17 +20,29 @@ function saveTransactions() {
 }
 
 function updateBalance() {
+
   let total = 0;
+  let incomeTotal = 0;
+  let expenseTotal = 0;
 
   transactions.forEach((transaction) => {
+
     if (transaction.type === "income") {
+
       total += transaction.amount;
+      incomeTotal += transaction.amount;
+
     } else {
+
       total -= transaction.amount;
+      expenseTotal += transaction.amount;
     }
+
   });
 
   balance.textContent = `₦${total}`;
+  income.textContent = `₦${incomeTotal}`;
+  expense.textContent = `₦${expenseTotal}`;
 }
 
 function renderTransactions() {
